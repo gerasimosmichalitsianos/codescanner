b086b085ac23d5b055f4155d91b3b6b24d22a2eb
Gerasimos Michalitsianos <gerasimosmichalitsianos@gmail.com>
2021-01-10 15:29:42 -0500

    Update setup.py

--- codescanner/setup.py
+++ codescanner/setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='codescanner',
     version='1.0.0',
-    scripts=['bin/codescanner.py','bin/fortranscanner.py','bin/ccppscanner.py',], 
+    scripts=['bin/codescanner.py','bin/fortranscanner.py','bin/ccppscanner.py','bin/scanner.py',], 
     license='MIT',
     include_package_data=True, 
     long_description=open('README.txt').read(),
     entry_points = {
-        'console_scripts': ['directoryscanner=directoryscanner:main'],
+        'console_scripts': ['codescanner=codescanner:main'],
     }
 	
 )
